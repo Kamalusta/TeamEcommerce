c40832cf27e4cd331355aa05411de842e1e0c255
khurbanli <fatimakhurbanli@gmail.com>
2024-08-02 11:45:53 +0400

    Add

--- TeamEcommerce/Abstract/ICategoryService.cs
+++ TeamEcommerce/Abstract/ICategoryService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.test
+namespace Business.Abstract
 {
-    internal class Test
+    public interface ICategoryService
     {
     }
 }
---
+++ TeamEcommerce/Concrete/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
